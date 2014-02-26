9a08adc673b53cc13feb0cced6e0bf96744d87ac
Christian Bosshard <cbossi1987@gmail.com>
2014-02-26 15:30:51 +0100

    Documentation

--- hellworld/src/ch/uzh/ifi/mp1401/hw/main/Main.java
+++ hellworld/src/ch/uzh/ifi/mp1401/hw/main/Main.java
@@ -2,6 +2,9 @@ package ch.uzh.ifi.mp1401.hw.main;
 
 public class Main {
 
+  /**
+   * Simply Hello World!
+   */
   public static void main(final String[] args) {
     System.out.println("Hello World!");
   }
